0ba5f083f7fbd77d009b2eb3bdf5d5f0f0488bfe
Vinicius <148116348+Sutaideru@users.noreply.github.com>
2025-09-15 10:47:16 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+    $servidor = "localhost";
+    $user ="root";
+    $password = "";
+    $db = "agendada";
+
+    $conection = mysqli_connect($servidor, $user, $password, $db);
+?>
---
+++ register_db.php
@@ -0,0 +1,11 @@
+<?php
+
+    include("conexao.php");
+
+    $nome = $_POST["nome"];
+    $disciplina = $_POST["disciplina"];
+    $email = $_POST["email"];
+    $matricula = $_POST[""];
+
+
+?>
