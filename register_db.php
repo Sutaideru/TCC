0f6a5fd43e0648042877c7f0a7a2d3552db73171
VitoDoSenai <vitodosenai@gmail.com>
2025-09-22 10:21:47 -0300

    Register DB atualizado 4.0

--- register_db.php
+++ register_db.php
@@ -3,22 +3,22 @@
     include("conexao.php");
     // Table professores
     $matricula = ["matricula"]
-    $nomeprof = $_POST["nomeprof"];
-    $turns = $_POST["turns"];
-    $curs = $_POST["curs"];
-    $competen = $_POST["competen"]
+    $nome_professor = $_POST["nome_professor"];
+    $turnos_professor = $_POST["turnos_professor"];
+    $cursos_professor = $_POST["cursos_professor"];
+    $competencias_professor = $_POST["competencias_professor"]
     //Tabela competências
     $IDcompetencia = $_POST["IDcompetencia"]
-    $nomecompetencia = $_POST["nomecompetencia"]
-    $cargahor = $_POST["cargaHr"]
-    $profess = $_POST["profess"]
-    $cursos = $_POST["cursos"]
+    $nome_competencia = $_POST["nome_competencia"]
+    $carga_horaria = $_POST["carga_horaria"]
+    $professores_competencia = $_POST["professores_competencia"]
+    $cursos_competencia = $_POST["cursos_competencia"]
     // Tabela cursos
     $IDcurso = ["IDcurso"]
-    $nomecurso = ["nomecurso"]
-    $turnos = ["turnos"]
-    $profes = ["profes"]
-    $compet = ["compet"]
+    $nome_curso = ["nome_curso"]
+    $turnos_curso = ["turnos_curso"]
+    $professores_curso = ["professores_curso"]
+    $competencias_curso = ["competencias_curso"]
     // Tabela login
     $usuario = ["usuario"] 
     $senha = ["senha"]
