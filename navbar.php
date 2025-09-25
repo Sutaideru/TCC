ab956fedef9daac6717ea894950cb1fead9e747f
VitoDoSenai <vitodosenai@gmail.com>
2025-09-25 08:42:07 -0300

    Navbar agora é um arquivo separado

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Bootstrap/bootstrap.css" rel="stylesheet">
+    <script src="Bootstrap/bootstrap.js"></script> 
+    <script src="scripts.js"></script>
+</head>
+<body>
+<ul class="nav" style = "background-color: #005caa;">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="home.php" style="color:white;">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="novo_prof.php" style="color:white;">Novo Prof</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="prof_add.php" style="color:white;">Prof Add</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="index.php" style="color:white;">Index</a>
+  </li>
+</ul>
+</body>
+</html>
