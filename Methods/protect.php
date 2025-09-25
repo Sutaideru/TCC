848346300831cc9032b1c551f01f8fbdcee1e6c4
Vinicius <148116348+Sutaideru@users.noreply.github.com>
2025-09-25 19:35:14 -0300

    Update protect.php

--- Methods/protect.php
+++ Methods/protect.php
@@ -12,18 +12,18 @@ if (!isset($_SESSION['user'])){
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <link href='Bootstrap/bootstrap.css' rel='stylesheet'>
     <script src='Bootstrap/bootstrap.js'></script> 
-    <script src='scripts.js'></script>
+    <script src='./Methods/scripts.js'></script>
     <link rel='icon' type='image/x-icon' href='./images/calendario.ico'>
     <title>Agenda</title>
     <meta>
         <style>
       body {
         background-image: url('./images/nome.png');
-        display: flex; /* Flexbox para centralizar */
-        justify-content: center; /* Centraliza horizontalmente */
-        align-items: center; /* Centraliza verticalmente */
-        height: 100vh; /* Garante que o body tenha a altura da janela */
-        margin: 0; /* Remove margens do body */
+        display: flex; 
+        justify-content: center; 
+        align-items: center; 
+        height: 100vh;
+        margin: 0; 
       }
     </style>
 </head>
@@ -36,3 +36,4 @@ if (!isset($_SESSION['user'])){
 }
 
 ?>
+
