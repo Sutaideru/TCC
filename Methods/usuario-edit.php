82c1da14303a0e0b8ef99f9f58afd90c6db59c81
Vinicius <148116348+Sutaideru@users.noreply.github.com>
2025-10-14 07:51:51 -0300

    Add files via upload

---
+++ Methods/usuario-edit.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    include 'navbar.php';
+    ?>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class = "row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4> Editar Professor
+                            <a href="index.php" style="btn btn-danger float-end"></a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
