08e54607539e3b2be65b69b622c69647f4562ad3
Vinicius <148116348+Sutaideru@users.noreply.github.com>
2025-10-14 11:36:50 -0300

    Add files via upload

---
+++ Methods/acoes.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require 'conexao.php';
+
+if (isset($_POST['create_usuario'])){
+    $matricula = mysqli_real_escape_string($connection, trim($_POST['matricula']));
+    $nome_professor = mysqli_real_escape_string($connection, trim($_POST['nome_professor']));
+    $turnos_professor = mysqli_real_escape_string($connection, trim($_POST['turnos_professor']));
+
+    $sql = "INSERT INTO professores (matricula, nome_professor, turnos_professor) VALUES ('$matricula', '$nome_professor', '$turnos_professor')";
+
+    mysqli_query($connection, $sql);
+
+    if (mysqli_affected_rows($connection) > 0){
+        $_SESSION['mensagem'] = 'Professor adicionado com sucesso';
+        header('Location: home.php');
+        exit;
+    } else {
+        $_SESSION['mensagem'] = 'Erro ao adicionar professor';
+        header('Location: home.php');
+        exit;
+    }
+}
+
+if (isset($_POST['update_usuario'])){
+    $usuario_id = mysqli_real_escape_string($connection, $_POST['usuario_id']);
+
+    $matricula = mysqli_real_escape_string($connection, trim($_POST['matricula']));
+    $nome_professor = mysqli_real_escape_string($connection, trim($_POST['nome_professor']));
+    $turnos_professor = mysqli_real_escape_string($connection, trim($_POST['turnos_professor']));
+
+    $sql = "UPDATE professores SET matricula = '$matricula', nome_professor = '$nome_professor', turnos_professor = '$turnos_professor' WHERE matricula = '$usuario_id'";
+
+    mysqli_query($connection, $sql);
+
+    if (mysqli_affected_rows($connection) > 0){
+        $_SESSION['mensagem'] = 'Professor atualizado com sucesso';
+        header('Location: home.php');
+        exit;
+    } else {
+        $_SESSION['mensagem'] = 'Erro ao atualizar professor';
+        header('Location: home.php');
+        exit;
+    }
+}
+?>
