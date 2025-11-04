49fc6db23f47e754cbfffe55e648b0fef2c30510
VitoDoSenai <vitodosenai@gmail.com>
2025-11-04 07:59:02 -0300

    updated

--- Pages/home.php
+++ Pages/home.php
@@ -1,39 +1,35 @@
 <?php
-  include "protect.php";
-  include 'navbar.php';
+include "protect.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="Bootstrap/bootstrap.css" rel="stylesheet">
-    <script src="Bootstrap/bootstrap.js"></script> 
-    <script src="scripts.js"></script>
-    <link rel="icon" type="image/x-icon" href="./images/calendario.ico">
-    <title>Agenda</title>
-    <script src='js/index.global.min.js'></script>
-    <link href="css/custom.css" rel="stylesheet">
-    <script>
-      document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        });
-        calendar.render();
-      });
-    </script>
-    <style>
-         body {
-            margin: 0;
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            background: linear-gradient(135deg, #f0f4f8, #d9e2ec);
-            min-height: 100vh;
-            flex-direction: column;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="Bootstrap/bootstrap.css" rel="stylesheet">
+  <script src="Bootstrap/bootstrap.js"></script>
+  <script src="scripts.js"></script>
+  <link rel="icon" type="image/x-icon" href="./images/calendario.ico">
+  <title>Agenda</title>
+  <script src='js/index.global.min.js'></script>
+  <script src='js/pt-br.global.min.js'></script>
+  <script src='js/custom.js'></script>
+  <link href="css/custom.css" rel="stylesheet">
+  <style>
+    body {
+      margin: 0;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      background: linear-gradient(135deg, #f0f4f8, #d9e2ec);
+      min-height: 100vh;
+      flex-direction: column;
+    }
+  </style>
 </head>
+
 <body>
-    <div id="calendar" style="margin-top: 5vh;"></div>
+  <?php include 'navbar.php'; ?>
+  <div id="calendar" style="margin-top: 2vh;"></div>
 </body>
+
 </html>
