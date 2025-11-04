0d943654d6745b1ea703f44292e7b40b0f0a7159
Vinicius <148116348+Sutaideru@users.noreply.github.com>
2025-11-04 11:24:32 -0300

    Add files via upload

--- Methods/navbar.php
+++ Methods/navbar.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="Bootstrap/bootstrap.css" rel="stylesheet">
-    <script src="Bootstrap/bootstrap.js"></script> 
+    <script src="Bootstrap/bootstrap.js"></script>
     <script src="scripts.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <style>
       .nav-item:hover{
         background-color: #0070d1ff;
@@ -20,21 +21,19 @@
 <body>
 <ul class="nav" style = "background-color: #005caa;">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="home.php" style="color:white;">Home</a>
+    <a class="nav-link active" aria-current="page" href="home.php" style="color:white;"><i class="bi bi-house-door-fill"></i>   Home</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="prof_add.php" style="color:white;">Professores</a>
+    <a class="nav-link" href="prof_add.php" style="color:white;"><i class="bi bi-people-fill"></i>   Professores</a>
   </li>
   <li>
-    <a class="nav-link active" aria-current="page" href="uc_add.php" style="color:white;">Unidades Curriculares</a>
+    <a class="nav-link active" aria-current="page" href="uc_add.php" style="color:white;"><i class="bi bi-book-half"></i>   Unidades Curriculares</a>
   </li>
     <li>
-    <a class="nav-link active" aria-current="page" href="conta.php" style="color:white;">Conta</a>
+    <a class="nav-link active" aria-current="page" href="conta.php" style="color:white;"><i class="bi bi-person-circle"></i>   Conta</a>
   </li>
   <li>
-    <a class="nav-link active" aria-current="page" href="logout.php" style="color:white;">
-      Logout
-    </a>
+    <a class="nav-link active" aria-current="page" href="logout.php" style="color:white;"><i class="bi bi-door-open-fill"></i>   Logout</a>
   </li>
 </ul>
 </body>
