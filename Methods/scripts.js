c5912bbe8e4c71f9d2d386f6eb187f03c2649712
Vinicius <148116348+Sutaideru@users.noreply.github.com>
2025-11-05 07:37:16 -0300

    Add files via upload

--- Methods/scripts.js
+++ Methods/scripts.js
@@ -13,3 +13,35 @@ function mostrarSenha(){
 		icon.classList.add('bi-eye-fill');
 	}
 }
+
+function mostrarNovaSenha(){
+	var inputPass = document.getElementById('nova_senha');
+	
+	var icon = document.getElementById('btn-senha-nova').querySelector('i'); 
+
+	if (inputPass.type === 'password') {
+		inputPass.setAttribute('type', 'text');
+		icon.classList.remove('bi-eye-fill');
+		icon.classList.add('bi-eye-slash-fill');
+	} else {
+		inputPass.setAttribute('type', 'password');
+		icon.classList.remove('bi-eye-slash-fill');
+		icon.classList.add('bi-eye-fill');
+	}
+}
+
+function mostrarConfirmarSenha(){
+	var inputPass = document.getElementById('confirmar_senha');
+	
+	var icon = document.getElementById('btn-senha-confirmar').querySelector('i'); 
+
+	if (inputPass.type === 'password') {
+		inputPass.setAttribute('type', 'text');
+		icon.classList.remove('bi-eye-fill');
+		icon.classList.add('bi-eye-slash-fill');
+	} else {
+		inputPass.setAttribute('type', 'password');
+		icon.classList.remove('bi-eye-slash-fill');
+		icon.classList.add('bi-eye-fill');
+	}
+}
